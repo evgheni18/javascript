82d016ec1be111a53b3456eb9274b70b8e63f06c
Eugen <eugencolun@gmail.com>
2022-10-05 00:55:18 +0300

    style

--- 2.2 Stilul de cod.js
+++ 2.2 Stilul de cod.js
@@ -31,39 +31,9 @@
 //   }
 //   alert(pow (6,2))
 
-// function pow(x, n) {
-//     if (n < 0) {
-//       alert(Valorile n negative nu sunt acceptate);
-//     } else {
-//       let result = 1;
-  
-//       for (let i = 0; i < n; i++) {
-//         result *= x;
-//       }
-  
-//       return result;
-//     }
-//   }
-
-// function pow(x, n) {
-//     if (n < 0) {
-//       alert(pow(1,2));
-//       return;
-//     }
-  
-//     let result = 1;
-  
-//     for (let i = 0; i < n; i++) {
-//       result *= x;
-//     }
-  
-//     return result;
-//   }
-  
-
 function pow(x, n) {
     if (n < 0) {
-      alert("Negative 'n' not supported");
+      alert(Valorile n negative nu sunt acceptate);
     } else {
       let result = 1;
   
@@ -74,4 +44,34 @@ function pow(x, n) {
       return result;
     }
   }
+
+function pow(x, n) {
+    if (n < 0) {
+      alert(pow(1,2));
+      return;
+    }
   
+//     let result = 1;
+  
+//     for (let i = 0; i < n; i++) {
+//       result *= x;
+//     }
+  
+//     return result;
+//   }
+  
+// recap
+
+// function pow(x, n) {
+//     if (n < 0) {
+//       alert("Negative 'n' not supported");
+//     } else {
+//       let result = 1;
+  
+//       for (let i = 0; i < n; i++) {
+//         result *= x;
+//       }
+  
+//       return result;
+//     }
+//   }
