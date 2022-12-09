7f17c31d401036dcff984dd24f215aa28a83af33
Evgheni18 <eugencolun@gmail.com>
2022-12-09 23:37:41 +0200

    09.12.2022

--- hello/src/App.js
+++ hello/src/App.js
@@ -14,14 +14,14 @@ import NameList from './components/NameList';
 import Person from './components/Person';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css';
+import Calculator from './components/Calculator';
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <h1 className = 'error'>Error</h1>
-      <h1 className= {styles.success}>Success</h1>
+      <Calculator />
+    {/* //   <h1 className = 'error'>Error</h1>
+    //   <h1 className= {styles.success}>Success</h1> */}
       {/* <Stylesheet primary = {true}/> */}
       {/* <Inline /> */}
       {/* <NameList /> */}
